efd79086070f65e2750cba0d929f638dc02d286b
Aleksey <aleksey18rus@inbox.ru>
2022-12-02 09:18:29 +0300

    make class stringComparer

--- logParser.py
+++ logParser.py
@@ -1,5 +1,5 @@
 import gzip
-import stringComparer
+from stringComparer import stringComparer
 
 
 def main():
--- stringComparer.py
+++ stringComparer.py
@@ -1,23 +1,24 @@
 import re
 
 
-def Contains(pattern: str, compared: str):
-    match = re.search(pattern, compared)
-    return True if match else False
+class stringComparer:
 
+    def Contains(pattern: str, compared: str):
+        match = re.search(pattern, compared)
+        return True if match else False
 
-def SplitByOrder(patternList: "list[str]", stringToSplit: str):
-    allFound = True
-    arr: list[str] = []
-    for index, value in enumerate(patternList):
-        match = re.search(value, stringToSplit)
-        if match:
-            arr.append(match[0])
-            stringToSplit = re.sub(value, "", stringToSplit, 1)
-        else:
-            arr.append("")
-            allFound = False
-    return arr, allFound
+    def SplitByOrder(patternList: "list[str]", stringToSplit: str):
+        allFound = True
+        arr: list[str] = []
+        for index, value in enumerate(patternList):
+            match = re.search(value, stringToSplit)
+            if match:
+                arr.append(match[0])
+                stringToSplit = re.sub(value, "", stringToSplit, 1)
+            else:
+                arr.append("")
+                allFound = False
+        return arr, allFound
 
 
 if __name__ == '__main__':
